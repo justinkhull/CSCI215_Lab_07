8fe6f3049215505a7be502bd855fda72064a96f1
justinkhull <hullj1@g.cofc.edu>
2017-10-30 23:54:05 -0400

    Complete Lab assignments (But had trouble with jumping multiple times)

--- mario_web_files/mario.js
+++ mario_web_files/mario.js
@@ -9,6 +9,7 @@ var Mario;
 
 
 window.onload = init; // calls the function named "init"
+//window.onload = document.getElementById(marioSound).play();
 // declare the background image
 var bgImage = new Image();
 
@@ -56,7 +57,7 @@ function draw() {
 	/*
 	 * TODO: Draw Mario's initial image
 	 */
-
+	Mario.Image.src = "mario1.png";
 
 	/////////////////////////////////////////////////////////////////
 	var render = function () {
@@ -73,18 +74,43 @@ function draw() {
 			ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
 			// Change the y value each time 
 			Mario.y -= 5; // move 5 px up
+			ctx.drawImage(bgImage, 0, 0);
+            ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
+            //clearInterval(Mario.timer);
+			console.log("First");
 		}else if(Mario.y <= 200 && Mario.moving == "up"){
 			Mario.moving = "down";
-		} else if(Mario.y < 280 && Mario.moving == "down"){
+			Mario.y += 5;
+            console.log("Second");
+		} else if(Mario.y < 615 && Mario.moving == "down"){
 			Mario.Image.src = "mario2.png";
+            ctx.drawImage(bgImage, 0, 0);
 			ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
-			Mario.y += 5; // move 5 px back down after a jump
-		}else if(Mario.y == 280 && Mario.moving == "no"){
+			Mario.y += 5;
+			// move 5 px back down after a jump
+			/*if (Mario.y == 615)
+			{
+				Mario.moving = "no";
+			}*/
+            console.log("Third");
+		}else if(Mario.y == 615 && Mario.moving == "no"){
 			Mario.moving = "up";
 			Mario.JumpSound.play();
-		}else{
+			console.log("HERE");
+		}
+		else if (Mario.y ==615 && Mario.moving == "down")
+		{
+			Mario.Image.src = "mario1.png";
+            ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
+            //Mario.moving = "up";
+			console.log("Fourth");
+			console.log(Mario.y, Mario.moving);
+
+		}
+		else{//*****************Initial jump from clouds***************************************
 			Mario.moving = "no";
 			Mario.Image.src = "mario1.png";
+			Mario.y += 335;
 			ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
 			clearInterval(Mario.timer); // kills the timer
 		}	
@@ -103,24 +129,52 @@ function draw() {
 	 */
 	document.body.onkeydown = function(e) {  // listen for a key
 
-    	e = event || window.event;             // any kind of event
-    	var keycode = e.charCode || e.keyCode; // any kind of key
-		console.log(keycode);
-		// The user wants Mario to jump:
-    	if(keycode === 13 && Mario.moving == "no") {  
-        	Mario.timer = setInterval(render, Mario.timerInterval); 
-    	}
+        e = event || window.event;             // any kind of event
+        var keycode = e.charCode || e.keyCode; // any kind of key
+        console.log(keycode);
+        // The user wants Mario to jump:
+        if (keycode === 13 && Mario.moving == "no") {
+            Mario.timer = setInterval(renderMario, Mario.timerInterval);
+        }
 
+        else if (keycode === 76 && Mario.x >= 10) {
+            Mario.Image.src = "left.png";
 
+            //ctx.clearRect(Mario.x, Mario.y + 335, Mario.w, Mario.h);
 
+                Mario.x -= 20;
+                ctx.drawImage(bgImage, 0, 0);
+                ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
+                clearInterval(Mario.timer);
+                setTimeout(faceForward, 200);
+        }
+        else if (keycode === 82 && Mario.x <= 1000) {
+            Mario.Image.src = "right.png";
+
+            Mario.x += 20;
+            ctx.drawImage(bgImage, 0, 0);
+            ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
+            clearInterval(Mario.timer);
+            setTimeout(faceForward, 200);
     }
+
 
     /* TODO:
      * TODO: Capture keycodes for L and R. In each, set a timeout that calls a function
      * TODO: to face Mario forward after 200 ms. HINT: setTimeout(function, timeInMilliSecs)
      */
     document.body.onkeyup = function(e) {  // listen for a key
+		/*e = event || window.event;
+		var keycode = e.charCode || e.keyCode;
+		console.log(keycode);
 
+		if (keycode === 76)
+		{
+			Mario.Image.src = "left.png";
+
+            ctx.drawImage(Mario.Image, Mario.x, Mario.y + 335, Mario.w, Mario.h);
+            clearInterval(Mario.timer);
+		}*/
     }
 
 
@@ -128,7 +182,10 @@ function draw() {
      * TODO: Face Mario forward. Do not forget to draw the background image first
      */
     function faceForward() {
-
+		Mario.Image.src = "mario1.png";
+        ctx.drawImage(bgImage, 0, 0);
+        ctx.drawImage(Mario.Image, Mario.x, Mario.y, Mario.w, Mario.h);
     }
 	
-} // close draw() 
+}
+}// close draw()
